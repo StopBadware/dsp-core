5dfca0d94f90ddbf4bc04f5b30abf5e0a883e82d
Matthew Frost <mfrost@stopbadware.org>
2012-12-26 16:42:54 -0500

    DATA-42 added Jersey

--- src/org/stopbadware/clearinghouse/JAXtest.java
+++ src/org/stopbadware/clearinghouse/JAXtest.java
@@ -1,8 +1,17 @@
 package org.stopbadware.clearinghouse;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/jax")
 public class JAXtest {
 
-	public String test(String testString) {
-		return ">>" + testString + "<<";
+	@GET
+	@Path("/{param}")
+	public Response test(@PathParam("param") String testString) {
+		String response = ">>" + testString + "<<";
+		return Response.status(200).entity(response).build();
 	}
 }
