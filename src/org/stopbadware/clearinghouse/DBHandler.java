31d61a13e0d28511235b850f788683685494f16c
Matthew Frost <mfrost@stopbadware.org>
2012-12-26 16:18:05 -0500

    DATA-42 created JAXRS JAXB test class

--- src/org/stopbadware/clearinghouse/DBHandler.java
+++ src/org/stopbadware/clearinghouse/DBHandler.java
@@ -2,8 +2,6 @@ package org.stopbadware.clearinghouse;
 
 public class DBHandler {
 	
-	public String test(String testString) {
-		return ">>" + testString + "<<";
-	}
+	
 
 }
---
+++ src/org/stopbadware/clearinghouse/JAXtest.java
@@ -0,0 +1,8 @@
+package org.stopbadware.clearinghouse;
+
+public class JAXtest {
+
+	public String test(String testString) {
+		return ">>" + testString + "<<";
+	}
+}
