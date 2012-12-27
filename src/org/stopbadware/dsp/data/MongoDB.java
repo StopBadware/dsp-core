49a58c8bbba063d0a19fc4ec7093bcad99e1f633
Matthew Frost <mfrost@stopbadware.org>
2012-12-27 11:08:22 -0500

    DATA-42 added MongoDB class

---
+++ src/org/stopbadware/dsp/data/MongoDB.java
@@ -0,0 +1,52 @@
+package org.stopbadware.dsp.data;
+
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+
+public class MongoDB {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(MongoDB.class);
+	private static MongoDB instance = null;
+	private Mongo m;
+	private DB db;	
+//	private static final String PRODUCTION_DB = "clearinghouse";
+//	private static final String STAGING_DB = "stagedb";
+//	private static final String TESTING_DB = "testdb";
+	private static final String DEVELOPMENT_DB = "devdb";
+
+	public static final String EVENT_REPORTS = "event_reports";
+	public static final String HOSTS = "hosts";
+	public static final String IPS = "ips";
+	public static final String ASNS = "autonomous_systems";
+	public static final String SOURCES = "data_sources";
+
+	public static final String DATE_PATTERN = "EEE MMM dd HH:mm:ss zzz yyyy";
+	public static final int ASC = 1;
+	public static final int DESC = -1;
+	
+	protected MongoDB() {
+		try {
+			m = new Mongo();
+			db = m.getDB(DEVELOPMENT_DB);
+		} catch (UnknownHostException | MongoException e) {
+			LOG.error("Unable to access database:\t{}", e.getMessage());
+		}
+	}
+	
+	public static MongoDB getInstance() {
+		if (instance == null) {
+			instance = new MongoDB();
+		}
+		return instance;
+	}
+	
+	public DB getDB() {
+		return db;
+	}	
+}
--- src/org/stopbadware/dsp/rest/All.java
+++ src/org/stopbadware/dsp/rest/All.java
@@ -5,21 +5,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.stopbadware.dsp.data.Delme;
 
-@Path("/jax")
-public class JAXtest {
+@Path("/all")
+public class All {
 
 	@GET
-	@Path("/{param}")
+	@Path("/since/{param}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Delme test(@PathParam("param") String testString) {
-//		String response = ">>" + testString + "<<";
-//		return Response.status(200).entity(response).build();
 		Delme d = new Delme();
-		d.setA("foo");
+		d.setA("all data");
 		d.setB(testString);
 		return d;
 	}
