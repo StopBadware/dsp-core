d9793db9fefc1a030c8f7e7e9d74e111392918c4
Matthew Frost <mfrost@stopbadware.org>
2013-01-02 16:18:00 -0500

    DATA-42 POST functioning

--- src/org/stopbadware/dsp/rest/Add.java
+++ src/org/stopbadware/dsp/rest/Add.java
@@ -1,6 +1,6 @@
 package org.stopbadware.dsp.rest;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,10 +12,13 @@ public class Add {
 	
 	private static DBHandler dbh = new DBHandler();
 	
-	@GET
+	@POST
 	@Path("/events")
+//	@Consumes(MediaType.APPLICATION_JSON)	//TODO:DATA-42 revert
 	@Produces(MediaType.APPLICATION_JSON)
-	public String renameMe() {
+	public String addEvents() {
+//		curl -d '' http://127.0.0.1:8080/clearinghouse/add/events
+		System.out.println("foo");
 		return "AOK";
 	}
 
