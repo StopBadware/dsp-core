070a9324d6bd31850158a4f7cf3b3d14a6c2e913
Matthew Frost <mfrost@stopbadware.org>
2013-01-17 17:03:51 -0500

    DATA-51 data received from importer

--- src/org/stopbadware/dsp/rest/Add.java
+++ src/org/stopbadware/dsp/rest/Add.java
@@ -1,14 +1,24 @@
 package org.stopbadware.dsp.rest;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.stopbadware.dsp.data.DBHandler;
 
 @Path("/add")
 public class Add {
 	
 	private static DBHandler dbh = new DBHandler();
+	private static final Logger LOG = LoggerFactory.getLogger(Add.class);
 	
 	@POST
 	@Path("/events")
@@ -16,9 +26,27 @@ public class Add {
 	public String addEvents(String data) {
 //		curl -d '' http://127.0.0.1:8080/clearinghouse/add/events
 		System.out.println(data);	//DELME: DATA-51
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			ReportContainer reports = mapper.readValue(data, ReportContainer.class);
+			System.out.println(reports.reports.size());
+//			ReportContainer foo = new ReportContainer();
+//			System.out.println(foo.time);
+		} catch (IOException e) {
+			LOG.error("Error parsing JSON:\t{}", e.getMessage());
+		}
 		//TODO: DATA-51 put input data as HashSet<Map<String, Object>> via Jackson
 		//TODO: DATA-51 send set to	dbh.addToEventReports() 
 		return "AOK";
+	}
+	
+	public static class ReportContainer {
+		public int size;
+		public long time;
+		public Set<Map<String, Object>> reports;
+		public ReportContainer() {
+			time = 14L;
+		}
 	}
 
 }
