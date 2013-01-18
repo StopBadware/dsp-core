3a52e3d0f5997011c9ad8b7e6825c22209136a25
Matthew Frost <mfrost@stopbadware.org>
2013-01-18 16:55:47 -0500

    DATA-51 cleaned up import handling

--- src/org/stopbadware/dsp/rest/Add.java
+++ src/org/stopbadware/dsp/rest/Add.java
@@ -24,20 +24,16 @@ public class Add {
 	
 	@POST
 	@Path("/events")
-	@Consumes(MediaType.APPLICATION_JSON)	//TODO: DATA-51 revert or remove
+	@Consumes(MediaType.APPLICATION_JSON)
 	public String addEvents(String data) {
-//		curl -d '' http://127.0.0.1:8080/clearinghouse/add/events
 		System.out.println(data);	//DELME: DATA-51
 		ObjectMapper mapper = new ObjectMapper();
 		try {
 			ReportContainer reports = mapper.readValue(data, ReportContainer.class);
 			System.out.println(reports.reports.size());
-//			ReportContainer foo = new ReportContainer();
-//			System.out.println(foo.time);
 		} catch (IOException e) {
 			LOG.error("Error parsing JSON:\t{}", e.getMessage());
 		}
-		//TODO: DATA-51 put input data as HashSet<Map<String, Object>> via Jackson
 		//TODO: DATA-51 send set to	dbh.addToEventReports() 
 		return "AOK";
 	}
@@ -47,6 +43,7 @@ public class Add {
 		public long time;
 		public Set<Map<String, Object>> reports;
 		public ReportContainer() {
+			
 		}
 	}
 
