ce0a988729f46a8915f86b3e7d696d32ab0054e8
Matthew Frost <mfrost@stopbadware.org>
2013-02-06 15:52:38 -0500

    DATA-52 added ResolverRequest wrapper

---
+++ src/org/stopbadware/dsp/json/ResolverRequest.java
@@ -0,0 +1,29 @@
+package org.stopbadware.dsp.json;
+
+import java.util.Set;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+/**
+ * Serialization wrapper for marshaling/unmarhsaling which
+ * hosts and/or IPs the Resolver should resolve 
+ *
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ResolverRequest {
+	
+	private Set<String> hosts;
+	
+	public ResolverRequest() {
+		
+	}
+	
+	public ResolverRequest(Set<String> hosts) {
+		this.hosts = hosts;
+	}
+	
+	public Set<String> getHosts() {
+		return hosts;
+	}
+
+}
