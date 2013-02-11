7a2817e82b1e322f11da50ae6380e60540a4b3fa
Matthew Frost <mfrost@stopbadware.org>
2013-02-11 13:44:07 -0500

    DATA-52 added switch for testing db

--- src/org/stopbadware/dsp/data/MongoDB.java
+++ src/org/stopbadware/dsp/data/MongoDB.java
@@ -17,18 +17,18 @@ public class MongoDB {
 	private DB db;	
 //	private static final String PRODUCTION_DB = "clearinghouse";
 //	private static final String STAGING_DB = "stagedb";
-//	private static final String TESTING_DB = "testdb";
+	private static final String TESTING_DB = "testdb";
 	private static final String DEVELOPMENT_DB = "devdb";
 
-	/*public static final String EVENT_REPORTS = "event_reports";
+	public static final String EVENT_REPORTS = "event_reports";
 	public static final String HOSTS = "hosts";
 	public static final String IPS = "ips";
-	public static final String ASNS = "autonomous_systems";*/	//TODO: DATA-51 revert
-	public static final String EVENT_REPORTS = "testCollection";
-	public static final String HOSTS = "testHosts";
-	public static final String IPS = "testCollection";
-	public static final String ASNS = "testCollection";
-	public static final String SOURCES = "data_sources";
+	public static final String ASNS = "autonomous_systems";	
+//	public static final String EVENT_REPORTS = "testCollection";	//DELME: DATA-50
+//	public static final String HOSTS = "testHosts";
+//	public static final String IPS = "testIPs";
+//	public static final String ASNS = "testCollection";
+//	public static final String SOURCES = "data_sources";
 
 	public static final int ASC = 1;
 	public static final int DESC = -1;
@@ -47,6 +47,13 @@ public class MongoDB {
 			instance = new MongoDB();
 		}
 		return instance;
+	}
+	
+	/**
+	 * Switches the database for use in unit testing
+	 */
+	public void switchToTestDB() {
+		db = m.getDB(TESTING_DB);
 	}
 	
 	/**
