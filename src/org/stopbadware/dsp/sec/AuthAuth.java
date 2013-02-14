c537569b474b0ea9b7914db630035fc404b842d4
Matthew Frost <mfrost@stopbadware.org>
2013-02-14 11:06:57 -0500

    DATA-54 sec manager through factory

--- src/org/stopbadware/dsp/sec/AuthAuth.java
+++ src/org/stopbadware/dsp/sec/AuthAuth.java
@@ -3,7 +3,9 @@ package org.stopbadware.dsp.sec;
 import javax.ws.rs.core.HttpHeaders;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
 
 /**
  * Authentication and authorization handler 
@@ -19,8 +21,14 @@ public abstract class AuthAuth {
 		System.out.println(key);
 		System.out.println(ts);
 		System.out.println(sig);
+		
+		Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory();
+		org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+		SecurityUtils.setSecurityManager(securityManager);
 		Subject subject = SecurityUtils.getSubject();
-		System.out.println(subject.isAuthenticated());
+		System.out.println(subject.toString());
+		
+		
 		return false;
 	}
 }
