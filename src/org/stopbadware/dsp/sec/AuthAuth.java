36c6023874ac03156fbc0aa16fbb3a4c0b2ab730
Matthew Frost <mfrost@stopbadware.org>
2013-02-19 11:37:07 -0500

    DATA-54 added auth fail logging

--- src/org/stopbadware/dsp/sec/AuthAuth.java
+++ src/org/stopbadware/dsp/sec/AuthAuth.java
@@ -8,6 +8,8 @@ import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.stopbadware.lib.util.SHA2;
 
 /**
@@ -16,30 +18,30 @@ import org.stopbadware.lib.util.SHA2;
 public abstract class AuthAuth {
 	
 	private static Realm realm = new Realm();
+	private static SecurityManager securityManager = new DefaultSecurityManager(realm);
+	private static final Logger LOG = LoggerFactory.getLogger(AuthAuth.class);
 	public static String REALMNAME = "SBW-DSP";
 
 	public static boolean authenticated(HttpHeaders httpHeaders, String path) {
 		String key = httpHeaders.getRequestHeaders().getFirst("SBW-Key");
 		String sig = httpHeaders.getRequestHeaders().getFirst("SBW-Signature");
 		long ts = Long.valueOf(httpHeaders.getRequestHeaders().getFirst("SBW-Timestamp"));
-		System.out.println(key);
-		System.out.println(sig);
-		System.out.println(ts);
-		System.out.println(path);
-		System.out.println(SHA2.get256(key+ts+path+"SECRET"));
-		SecurityManager securityManager = new DefaultSecurityManager(realm);
+		
 		SecurityUtils.setSecurityManager(securityManager);
 		Subject subject = SecurityUtils.getSubject();
 		AuthenticationToken token = new RESTfulToken(key, sig, path, ts); 
 		boolean authenticated = false;
+		
 		try {
 			subject.login(token);
 			System.out.println(subject.toString());
 			System.out.println(subject.isAuthenticated());
 			authenticated = subject.isAuthenticated();
 		} catch (AuthenticationException e) {
-			System.err.println(e.getMessage());
+			System.err.println(e.getMessage());	//DELME: DATA-54
+			LOG.warn("Authentication failure for API Key {}:\t{}", e.getMessage());
 		}
+		
 		return authenticated;
 	}
 }
