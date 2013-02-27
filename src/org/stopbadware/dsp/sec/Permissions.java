4da042ef08413d68f15e86ab9204c2051c260617
Matthew Frost <mfrost@stopbadware.org>
2013-02-27 16:46:07 -0500

    DATA-54 added Permissions constants

--- src/org/stopbadware/dsp/sec/Permissions.java
+++ src/org/stopbadware/dsp/sec/Permissions.java
@@ -1,11 +1,25 @@
 package org.stopbadware.dsp.sec;
 
+import org.stopbadware.dsp.data.MongoDB;
+
 /**
  * 
  *
  */
 public abstract class Permissions {
 
-	public static final String TEST_PERM = "testperm";
+	private static final String READ = "read";
+	private static final String WRITE = "write";
+	
+	public static final String READ_ASNS = MongoDB.ASNS+":"+READ;
+	public static final String READ_EVENTS = MongoDB.EVENT_REPORTS+":"+READ;
+	public static final String READ_HOSTS = MongoDB.HOSTS+":"+READ;
+	public static final String READ_IPS = MongoDB.IPS+":"+READ;
+	
+	public static final String WRITE_ACCOUNTS = MongoDB.ACCOUNTS+":"+WRITE;
+	public static final String WRITE_ASNS = MongoDB.ASNS+":"+WRITE;
+	public static final String WRITE_EVENTS = MongoDB.EVENT_REPORTS+":"+WRITE;
+	public static final String WRITE_HOSTS = MongoDB.HOSTS+":"+WRITE;
+	public static final String WRITE_IPS = MongoDB.IPS+":"+WRITE;
 	
 }
