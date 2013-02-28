654b2310bb6780774e8c293ba68cd9f7fa02198a
Matthew Frost <mfrost@stopbadware.org>
2013-02-28 12:32:16 -0500

    DATA-54 SecureREST jdoc update

--- src/org/stopbadware/dsp/rest/SecureREST.java
+++ src/org/stopbadware/dsp/rest/SecureREST.java
@@ -8,12 +8,22 @@ import org.apache.shiro.subject.Subject;
 import org.stopbadware.dsp.data.DBHandler;
 import org.stopbadware.dsp.sec.AuthAuth;
 
-public class SecureREST {
+/**
+ * Super-class ALL other classes in org.stopbadware.dsp.rest should extend. This
+ * class provides for retrieval of a DBHandler instance after authenticating   
+ */
+public abstract class SecureREST {
 	
 	@Context UriInfo uri;
 	@Context HttpHeaders httpHeaders;
 	protected Subject subject = AuthAuth.getEmptySubject();
 	
+	/**
+	 * Instantiates a DBHandler instance with a Shiro Subject created from
+	 * the required data found in the HTTP headers and the URI
+	 * @return a DBHandler instance with an authenticated subject, or null
+	 * if the authentication failed
+	 */
 	protected DBHandler getDBH() {
 		subject = AuthAuth.getSubject(httpHeaders, uri.getRequestUri());
 		if (subject.isAuthenticated()) {
