46aaefe16d8404d3ce83b584c2074d529efd781c
Matthew Frost <mfrost@stopbadware.org>
2013-02-28 15:07:37 -0500

    DATA-54 perm sharelevel var

--- src/org/stopbadware/dsp/sec/Permissions.java
+++ src/org/stopbadware/dsp/sec/Permissions.java
@@ -7,6 +7,7 @@ public abstract class Permissions {
 
 	private static final String READ = "read";
 	private static final String WRITE = "write";
+	private static final String SHARE_LEVEL = "share_level";
 	
 	public static final String READ_ASNS = MongoDB.ASNS+":"+READ;
 	public static final String READ_EVENTS = MongoDB.EVENT_REPORTS+":"+READ;
@@ -19,10 +20,10 @@ public abstract class Permissions {
 	public static final String WRITE_HOSTS = MongoDB.HOSTS+":"+WRITE;
 	public static final String WRITE_IPS = MongoDB.IPS+":"+WRITE;
 	
-	public static final String SL_SBW_ONLY = "share_level:"+ShareLevel.SBW_ONLY.toString().toLowerCase();
-	public static final String SL_DSP_ONLY = "share_level:"+ShareLevel.DSP_ONLY.toString().toLowerCase();
-	public static final String SL_RESEARCH = "share_level:"+ShareLevel.RESEARCH.toString().toLowerCase();
-	public static final String SL_TAKEDOWN = "share_level:"+ShareLevel.TAKEDOWN.toString().toLowerCase();
-	public static final String SL_PUBLIC = "share_level:"+ShareLevel.PUBLIC.toString().toLowerCase();
+	public static final String SL_SBW_ONLY = SHARE_LEVEL+":"+ShareLevel.SBW_ONLY.toString().toLowerCase();
+	public static final String SL_DSP_ONLY = SHARE_LEVEL+":"+ShareLevel.DSP_ONLY.toString().toLowerCase();
+	public static final String SL_RESEARCH = SHARE_LEVEL+":"+ShareLevel.RESEARCH.toString().toLowerCase();
+	public static final String SL_TAKEDOWN = SHARE_LEVEL+":"+ShareLevel.TAKEDOWN.toString().toLowerCase();
+	public static final String SL_PUBLIC = SHARE_LEVEL+":"+ShareLevel.PUBLIC.toString().toLowerCase();
 	
 }
