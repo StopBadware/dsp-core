effed4e6c9fb94961ab071b4e6ac9a1b8a30810b
Matthew Frost <mfrost@stopbadware.org>
2013-03-01 14:49:14 -0500

    DATA-54 secdbhandler unit testing

--- test/org/stopbadware/dsp/data/SecurityDBHandlerTest.java
+++ test/org/stopbadware/dsp/data/SecurityDBHandlerTest.java
@@ -2,15 +2,19 @@ package org.stopbadware.dsp.data;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authz.Permission;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
-import org.junit.Before;
 import org.junit.Test;
 import org.stopbadware.dsp.sec.RESTfulToken;
 import org.stopbadware.dsp.sec.Realm;
+import org.stopbadware.dsp.sec.Role;
 
 public class SecurityDBHandlerTest {
 	
@@ -18,17 +22,17 @@ public class SecurityDBHandlerTest {
 	private static SecurityDBHandler dbh = null; 
 	private static Realm realm = new Realm();
 	private static SecurityManager securityManager = new DefaultSecurityManager(realm);
+	private static final String TEST_APIKEY = "DATA123456";
 	
 	static {
 		realm.setCachingEnabled(false);
 		SecurityUtils.setSecurityManager(securityManager);
-		Subject subject = SecurityUtils.getSubject();
-		String key = "DATA123456";
+		subject = SecurityUtils.getSubject();
 		String sig = "54fc7ffd3cdc856c09c8747b61718741166f347b93f43c8db2ce6e4f568881e1";
 		String path = "/clearinghouse/events/test";
 		long ts = 1294513200L;
 		
-		RESTfulToken token = new RESTfulToken(key, sig, path, ts); 
+		RESTfulToken token = new RESTfulToken(TEST_APIKEY, sig, path, ts); 
 		try {
 			subject.login(token);
 		} catch (AuthenticationException e) {
@@ -38,43 +42,47 @@ public class SecurityDBHandlerTest {
 		dbh = new SecurityDBHandler();
 	}
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@Test
-	public void testSecurityDBHandler() {
-		fail("Not yet implemented");
-	}
-
 	@Test
 	public void testGetSecret() {
-		fail("Not yet implemented");
+		String secret = dbh.getSecret(subject.getPrincipal().toString());
+		assertTrue(secret != null);
+		assertTrue(secret.length() > 0);
 	}
 
 	@Test
 	public void testAddUser() {
-		fail("Not yet implemented");
+		Set<Role> roles = new HashSet<>();
+		String newAPIKey = dbh.addUser(roles, subject);
+		assertTrue(newAPIKey != null); 
+		assertTrue(newAPIKey.length() > 0);
 	}
 
 	@Test
 	public void testGetRoles() {
-		fail("Not yet implemented");
+		Set<String> roles = dbh.getRoles(TEST_APIKEY);
+		assertTrue(roles != null);
+		assertTrue(roles.size() > 0);
 	}
 
 	@Test
 	public void testGetObjectPermissions() {
-		fail("Not yet implemented");
+		Set<Permission> perms = dbh.getObjectPermissions(TEST_APIKEY);
+		assertTrue(perms != null);
+		assertTrue(perms.size() > 0);
 	}
 
 	@Test
 	public void testGetStringPermissions() {
-		fail("Not yet implemented");
+		Set<String> perms = dbh.getStringPermissions(TEST_APIKEY);
+		assertTrue(perms != null);
+		assertTrue(perms.size() > 0);
 	}
 
 	@Test
 	public void testGetRolePermissions() {
-		fail("Not yet implemented");
+		Set<String> perms = dbh.getRolePermissions(Role.ADMIN.toString());
+		assertTrue(perms != null);
+		assertTrue(perms.size() > 0);
 	}
 
 }
