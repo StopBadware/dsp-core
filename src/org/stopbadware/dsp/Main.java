d9a0480bc008f0716dbd48d2778f4e89271e8a7d
Matthew Frost <mfrost@stopbadware.org>
2013-03-08 10:58:30 -0500

    DATA-41 removed unneeded entry class

--- src/org/stopbadware/dsp/Main.java
+++ src/org/stopbadware/dsp/Main.java
@@ -3,25 +3,12 @@ package org.stopbadware.dsp;
 import java.io.File;
 
 import org.apache.catalina.startup.Tomcat;
-import org.stopbadware.lib.util.SHA2;
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/*import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;*/
 
 public class Main {
 	
     public static void main(String[] args) throws Exception {
 
         String webappDirLocation = "WebContent/";
-//        System.out.println("bar");
-//        System.out.println(SHA2.get256("yakabouche"));
         Tomcat tomcat = new Tomcat();
 
         //The port that we should run on can be set into an environment variable
@@ -39,21 +26,5 @@ public class Main {
         tomcat.start();
         tomcat.getServer().await();  
     }
-
-/*    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        resp.getWriter().print("Hello from Java!\n");
-    }
-
-    public static void main(String[] args) throws Exception{
-        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-        context.addServlet(new ServletHolder(new Main()),"/*");
-        server.start();
-        server.join();   
-    }*/
 
 }
