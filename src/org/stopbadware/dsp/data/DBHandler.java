0a93bf4de1ccfa7df98ea497863082b9433a5b95
Matthew Frost <mfrost@stopbadware.org>
2013-03-12 15:58:00 -0400

    DATA-63 ENV var test

--- src/org/stopbadware/dsp/data/DBHandler.java
+++ src/org/stopbadware/dsp/data/DBHandler.java
@@ -67,7 +67,7 @@ public class DBHandler {
 			LOG.debug("{}", cur.next());
 		}
 		LOG.debug("{}", cur.size());
-		LOG.debug("ENV:{}", System.getenv("mode"));
+		LOG.debug("ENV:{}", System.getenv("MODE"));
 		LOG.debug("END TEST");
 	}
 	
