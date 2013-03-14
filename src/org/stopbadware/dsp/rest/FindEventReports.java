40a324e7afe9db417e7e7cdc046cb0603ee84409
Matthew Frost <mfrost@stopbadware.org>
2013-03-14 12:21:11 -0400

    DATA-41 changed test to POST

--- src/org/stopbadware/dsp/rest/FindEventReports.java
+++ src/org/stopbadware/dsp/rest/FindEventReports.java
@@ -2,6 +2,7 @@ package org.stopbadware.dsp.rest;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -15,7 +16,8 @@ import org.stopbadware.dsp.sec.AuthAuth;
 @Path("/events")
 public class FindEventReports extends SecureREST {
 	
-	@GET
+//	@GET
+	@POST
 	@Path("/test")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
