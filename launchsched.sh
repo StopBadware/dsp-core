98ccde6c1fa6696052173381452c105ff57fbaf0
Matthew Frost <mfrost@stopbadware.org>
2013-03-20 10:49:22 -0400

    DATA-66 added scheduler launcher

--- launchsched.sh
+++ launchsched.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#! /bin/sh
+cp logback.xml.template ./target/classes/logback.xml
 classdir='"\$BASEDIR"\/classes:'
 generated_path='CLASSPATH=\$CLASSPATH_PREFIX:"\$BASEDIR"\/etc:'
-sed "s/\($generated_path\)/\1$classdir/" target/bin/webapp >webapp
 sed "s/\($generated_path\)/\1$classdir/" target/bin/scheduler >scheduler
-mv webapp target/bin/
 mv scheduler target/bin/
+sh target/bin/scheduler
