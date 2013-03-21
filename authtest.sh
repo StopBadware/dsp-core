05af7fae1c269af09a44064199bc6d813afee3c9
Matthew Frost <mfrost@stopbadware.org>
2013-03-21 15:00:22 -0400

    DATA-66 removed test/dev files

--- authtest.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-curl -v -H "SBW-Key:DATA123456" -H "SBW-Timestamp:1294513200" -H "SBW-Signature:54fc7ffd3cdc856c09c8747b61718741166f347b93f43c8db2ce6e4f568881e1" http://127.0.0.1:8080/clearinghouse/events/test
--- failtest.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-curl -v -H "SBW-Key:" -H "SBW-Timestamp:" -H "SBW-Signature:7c1a85b5698515fe25878803d65f6e8536767637b33402e9c1991cbc08b15d6e" http://127.0.0.1:8080/clearinghouse/events/test
