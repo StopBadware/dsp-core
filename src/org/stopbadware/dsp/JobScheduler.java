828a9974a03a15856776c6b51b265f914ee4e1e6
Matthew Frost <mfrost@stopbadware.org>
2013-04-02 11:13:34 -0400

    DATA-74 add messages to msg queues

--- src/org/stopbadware/dsp/JobScheduler.java
+++ src/org/stopbadware/dsp/JobScheduler.java
@@ -5,10 +5,6 @@ import io.iron.ironmq.Cloud;
 import io.iron.ironmq.Queue;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.quartz.Job;
 import org.quartz.JobDetail;
@@ -19,7 +15,6 @@ import org.quartz.Trigger;
 import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.stopbadware.lib.util.SHA2;
 
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerBuilder.newTrigger;
@@ -49,8 +44,6 @@ public class JobScheduler {
 	
 	public static class Import implements Job {
 
-		private static String impHost = (System.getenv("SBW_IMP_HOST")!=null) ? System.getenv("SBW_IMP_HOST") : "http://127.0.0.1";
-		
 		@Override
 		public void execute(JobExecutionContext context) throws JobExecutionException {
 			beginImporting();
@@ -68,40 +61,9 @@ public class JobScheduler {
 			}
 		}
 		
-		private void OLDbeginImporting() {		//DELME: DATA-74
-			try {
-				URL url = new URL(impHost+"/import/all/");
-				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-				conn.setRequestMethod("POST");
-				Map<String, String> authHeaders = createImporterAuthHeaders(url.getPath().toString());
-				for (String key : authHeaders.keySet()) {
-					conn.setRequestProperty(key, authHeaders.get(key));
-				}
-				int resCode = conn.getResponseCode();
-				if (resCode == 200) {
-					LOG.info("Begin importing [all] request sent to Importer");
-				} else {
-					LOG.error("Unable to connect to Importer, received HTTP Status Code {}", resCode);
-				}
-			} catch (IOException e) {
-				LOG.error("Unable to connect to Importer:\t{}", e.getMessage());
-			}
-		}
-		
-		private Map<String, String> createImporterAuthHeaders(String path) {
-			Map<String, String> headers = new HashMap<>();
-			String secret = (System.getenv("SBW_IMP_SECRET")!=null) ? System.getenv("SBW_IMP_SECRET") : "";
-			String timestamp = String.valueOf(System.currentTimeMillis() / 1000);
-			String signature = SHA2.get256(timestamp+secret);
-			headers.put("SBW-IMP-Timestamp", timestamp);
-			headers.put("SBW-IMP-Signature", signature);
-			return headers;
-		}
 	}
 	
 	public static class Resolve implements Job {
-		
-		private static String resHost = (System.getenv("SBW_RES_HOST")!=null) ? System.getenv("SBW_RES_HOST") : "http://127.0.0.1";
 		
 		@Override
 		public void execute(JobExecutionContext context) throws JobExecutionException {
@@ -120,35 +82,6 @@ public class JobScheduler {
 			}
 		}
 		
-		private void OLDbeginResolving() {		//DELME: DATA-74
-			try {
-				URL url = new URL(resHost+"/resolve/hosts/");
-				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-				conn.setRequestMethod("POST");
-				Map<String, String> authHeaders = createResolverAuthHeaders(url.getPath().toString());
-				for (String key : authHeaders.keySet()) {
-					conn.setRequestProperty(key, authHeaders.get(key));
-				}
-				int resCode = conn.getResponseCode();
-				if (resCode == 200) {
-					LOG.info("Begin resolving request sent to Resolver");
-				} else {
-					LOG.error("Unable to connect to resolver, received HTTP Status Code {}", resCode);
-				}
-			} catch (IOException e) {
-				LOG.error("Unable to connect to Resolver:\t{}", e.getMessage());
-			}	
-		}
-		
-		private Map<String, String> createResolverAuthHeaders(String path) {
-			Map<String, String> headers = new HashMap<>();
-			String secret = (System.getenv("SBW_RES_SECRET")!=null) ? System.getenv("SBW_RES_SECRET") : "";
-			String timestamp = String.valueOf(System.currentTimeMillis() / 1000);
-			String signature = SHA2.get256(timestamp+secret);
-			headers.put("SBW-RES-Timestamp", timestamp);
-			headers.put("SBW-RES-Signature", signature);
-			return headers;
-		}
 	}
 
 }
