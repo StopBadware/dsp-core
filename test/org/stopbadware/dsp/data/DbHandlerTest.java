2caf0cc460f0ceb27a4baf4feffce7851ac29b01
Matthew Frost <mfrost@stopbadware.org>
2013-10-04 12:33:24 -0400

    DATA-120 updated dbhandler tests

--- test/org/stopbadware/dsp/data/DbHandlerTest.java
+++ test/org/stopbadware/dsp/data/DbHandlerTest.java
@@ -7,70 +7,43 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.mgt.DefaultSecurityManager;
-import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.subject.Subject;
 import org.junit.Test;
 import org.stopbadware.dsp.ShareLevel;
 import org.stopbadware.dsp.json.AutonomousSystem;
 import org.stopbadware.dsp.json.ERWrapper;
 import org.stopbadware.dsp.json.TimeOfLast;
-import org.stopbadware.dsp.sec.RestToken;
-import org.stopbadware.dsp.sec.Realm;
+import org.stopbadware.dsp.test.helpers.AuthAuthTestHelper;
 import org.stopbadware.lib.util.SHA2;
 
 public class DbHandlerTest {
 	
-	private static DbHandler dbh = null; 
-	private static Realm realm = new Realm();
-	private static SecurityManager securityManager = new DefaultSecurityManager(realm);
-	private static final String TEST_SOURCE = "TESTING";
+	private static DbHandler dbh = new DbHandler(AuthAuthTestHelper.getSubject()); 
+	private static final String TEST_PREFIX = "TEST";
 	private static final String TEST_HOST = "example.com";
-	private static final long TEST_IP = 47L;
-	private static final int TEST_ASN = 18;
 	
-	static {
-		realm.setCachingEnabled(false);
-		SecurityUtils.setSecurityManager(securityManager);
-		Subject subject = SecurityUtils.getSubject();
-		String key = "DATA123456";
-		String sig = "54fc7ffd3cdc856c09c8747b61718741166f347b93f43c8db2ce6e4f568881e1";
-		String path = "/v0.2/events/since/0";
-		long ts = 1294513200L;
-		
-		RestToken token = new RestToken(key, sig, path, ts); 
-		try {
-			subject.login(token);
-		} catch (AuthenticationException e) {
-			fail("AuthenticationException thrown");
-		}
-		dbh = new DbHandler(subject);
-	}
-
 	@Test
-	public void testGetTimeOfLast() {
-		TimeOfLast tol = dbh.getTimeOfLast(TEST_SOURCE);
-		assertTrue(tol != null && tol instanceof TimeOfLast);
+	public void getTimeOfLastTest() {
+		TimeOfLast tol = dbh.getTimeOfLast(TEST_PREFIX);
+		assertTrue(tol != null);
 		assertTrue(tol.getLast() >= 0 && tol.getLast() < Long.MAX_VALUE);
-		assertTrue(tol.getSource().equalsIgnoreCase(TEST_SOURCE));
+		assertTrue(tol.getSource().equalsIgnoreCase(TEST_PREFIX));
 	}
 
 	@Test
-	public void testGetCurrentlyBlacklistedHosts() {
+	public void getCurrentlyBlacklistedHostsTest() {
 		Set<String> hosts = dbh.getCurrentlyBlacklistedHosts();
-		assertTrue(hosts != null && hosts instanceof HashSet);
+		assertTrue(hosts != null);
+		assertTrue(hosts.size() > 0);
 	}
 
 	@Test
-	public void testAddEventReports() {
+	public void addEventReportsTest() {
 		Set<ERWrapper> reports = new HashSet<>();
 		Map<String, Object> erMap = new HashMap<>();
 		erMap.put("sha2_256", SHA2.get256("A long time ago, in a galaxy far, far away..."));
-		erMap.put("reported_by", TEST_SOURCE);
+		erMap.put("prefix", TEST_PREFIX);
 		erMap.put("reported_at", System.currentTimeMillis()/1000);
-		erMap.put("host", TEST_HOST);
+		erMap.put("host", "");
 		ERWrapper erw = new ERWrapper(TEST_HOST, ShareLevel.PUBLIC.toString(), erMap);
 		reports.add(erw);
 		int added = dbh.addEventReports(reports);
@@ -78,36 +51,36 @@ public class DbHandlerTest {
 	}
 
 	@Test
-	public void testAddIPsForHosts() {
+	public void addIPsForHostsTest() {
 		Map<String, Long> ips = new HashMap<>();
 		ips.put(TEST_HOST, 0L);
 		dbh.addIPsForHosts(ips);
 		/* set to zero to ensure next call should result in a db write */
-		ips.put(TEST_HOST, TEST_IP);
+		ips.put(TEST_HOST, 1L);
 		int added = dbh.addIPsForHosts(ips);
 		assertTrue(added > 0);
 	}
 
 	@Test
-	public void testAddASNsForIPs() {
+	public void addASNsForIPsTest() {
 		Map<Long, AutonomousSystem> asns = new HashMap<>();
 		AutonomousSystem asn = new AutonomousSystem();
-		asn.setAsn(0);
-		asns.put(TEST_IP, asn);
+		asn.setAsn(AsnsHandlerTest.PRIVATE_AS_RANGE_START);
+		asns.put(0L, asn);
 		/* set to zero to ensure next call should result in a db write */
 		dbh.addASNsForIPs(asns);
-		asn.setAsn(TEST_ASN);
-		asns.put(TEST_IP, asn);
+		asn.setAsn(AsnsHandlerTest.PRIVATE_AS_RANGE_END);
+		asns.put(0L, asn);
 		int added = dbh.addASNsForIPs(asns);
 		assertTrue(added > 0);
 	}
 
 	@Test
-	public void testUpdateBlacklistFlagsFromCleanHosts() {
+	public void updateBlacklistFlagsFromCleanHostsTest() {
 		Set<ERWrapper> reports = new HashSet<>();
 		Map<String, Object> erMap = new HashMap<>();
 		erMap.put("sha2_256", SHA2.get256("Space, the final frontier, these are the voyages..."));
-		erMap.put("reported_by", TEST_SOURCE);
+		erMap.put("prefix", TEST_PREFIX);
 		erMap.put("reported_at", System.currentTimeMillis()/1000);
 		erMap.put("is_on_blacklist", true);
 		erMap.put("host", TEST_HOST);
@@ -117,7 +90,7 @@ public class DbHandlerTest {
 		if (added > 0) {
 			Set<String> cleanHosts = new HashSet<>();
 			cleanHosts.add(TEST_HOST);
-			int cleaned = dbh.updateBlacklistFlagsFromCleanHosts(TEST_SOURCE, System.currentTimeMillis()/1000, cleanHosts);
+			int cleaned = dbh.updateBlacklistFlagsFromCleanHosts(TEST_PREFIX, System.currentTimeMillis()/1000, cleanHosts);
 			assertTrue(cleaned > 0);
 		} else {
 			fail("Unable to add test event report");
@@ -125,12 +98,12 @@ public class DbHandlerTest {
 	}
 
 	@Test
-	public void testUpdateBlacklistFlagsFromDirtyReports() {
+	public void updateBlacklistFlagsFromDirtyReportsTest() {
 		Set<ERWrapper> reports = new HashSet<>();
 		/* to be cleaned */
 		Map<String, Object> erMap = new HashMap<>();
 		erMap.put("sha2_256", SHA2.get256("Making your way in the world today takes everything you got..."));
-		erMap.put("reported_by", TEST_SOURCE);
+		erMap.put("prefix", TEST_PREFIX);
 		erMap.put("reported_at", System.currentTimeMillis()/1000);
 		erMap.put("is_on_blacklist", true);
 		erMap.put("host", "cleanme.test");
@@ -145,14 +118,14 @@ public class DbHandlerTest {
 		/* dirty placeholder */
 		erMap = new HashMap<>();
 		erMap.put("sha2_256", SHA2.get256("So no one told you life was gonna be this way..."));
-		erMap.put("reported_by", TEST_SOURCE);
+		erMap.put("prefix", TEST_PREFIX);
 		erMap.put("reported_at", System.currentTimeMillis()/1000);
 		erMap.put("is_on_blacklist", true);
 		erMap.put("host", TEST_HOST);
 		ERWrapper newERW = new ERWrapper(TEST_HOST, ShareLevel.PUBLIC.toString(), erMap);
 		reports.add(newERW);
 		
-		int cleaned = dbh.updateBlacklistFlagsFromDirtyReports(TEST_SOURCE, System.currentTimeMillis()/1000, reports);
+		int cleaned = dbh.updateBlacklistFlagsFromDirtyReports(TEST_PREFIX, System.currentTimeMillis()/1000, reports);
 		assertTrue(cleaned > 0);
 	}
 
