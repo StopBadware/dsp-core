19d5a42affd77e50d5c7996cf66cd6d8170711cc
Matthew Frost <mfrost@stopbadware.org>
2013-10-04 16:55:55 -0400

    DATA-120 updating secdb tests

--- test/org/stopbadware/dsp/data/SecurityDbHandlerTest.java
+++ test/org/stopbadware/dsp/data/SecurityDbHandlerTest.java
@@ -5,83 +5,82 @@ import static org.junit.Assert.*;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authz.Permission;
-import org.apache.shiro.mgt.DefaultSecurityManager;
-import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.stopbadware.dsp.sec.RestToken;
-import org.stopbadware.dsp.sec.Realm;
 import org.stopbadware.dsp.sec.Role;
+import org.stopbadware.dsp.test.helpers.AuthAuthTestHelper;
 
 public class SecurityDbHandlerTest {
 	
-	private static Subject subject = null;
-	private static SecurityDbHandler dbh = null; 
-	private static Realm realm = new Realm();
-	private static SecurityManager securityManager = new DefaultSecurityManager(realm);
-	private static final String TEST_APIKEY = "DATA123456";
+	private static Subject subject = AuthAuthTestHelper.getSubject();
+	private static SecurityDbHandler dbh = new SecurityDbHandler(); 
+	private static final String TEST_APIKEY = "TEST_APIKEY";
+//	private static final String TEST_SECRET = "TEST_SECRET";
 	
-	static {
-		realm.setCachingEnabled(false);
-		SecurityUtils.setSecurityManager(securityManager);
-		subject = SecurityUtils.getSubject();
-		String sig = "54fc7ffd3cdc856c09c8747b61718741166f347b93f43c8db2ce6e4f568881e1";
-		String path = "/v0.2/events/since/0";
-		long ts = 1294513200L;
-		
-		RestToken token = new RestToken(TEST_APIKEY, sig, path, ts); 
-		try {
-			subject.login(token);
-		} catch (AuthenticationException e) {
-			fail("AuthenticationException thrown");
-		}
-		dbh = new SecurityDbHandler();
+	@BeforeClass
+	public static void addTestAccount() {
+		//TODO DATA-120
 	}
-
+	
 	@Test
-	public void testGetSecret() {
+	public void getSecretTest() {
 		String secret = dbh.getSecret(subject.getPrincipal().toString());
 		assertTrue(secret != null);
 		assertTrue(secret.length() > 0);
+		assertTrue(false);
+	}
+	
+	@Test
+	public void disableUserTest() {
+		assertTrue(false);
+	}
+	
+	@Test
+	public void getParticipantTest() {
+		assertTrue(false);
 	}
 
 	@Test
-	public void testAddUser() {
-		Set<Role> roles = new HashSet<>();
-		String newAPIKey = dbh.addUser(roles, subject, "test");
-		assertTrue(newAPIKey != null); 
-		assertTrue(newAPIKey.length() > 0);
+	public void addUserTest() {
+//		Set<Role> roles = new HashSet<>();
+//		String newAPIKey = dbh.addUser(roles, subject, "test");
+//		assertTrue(newAPIKey != null); 
+//		assertTrue(newAPIKey.length() > 0);
+		assertTrue(false);
 	}
 
 	@Test
-	public void testGetRoles() {
+	public void getRolesTest() {
 		Set<String> roles = dbh.getRoles(TEST_APIKEY);
 		assertTrue(roles != null);
 		assertTrue(roles.size() > 0);
+		assertTrue(false);
 	}
 
 	@Test
-	public void testGetObjectPermissions() {
+	public void getObjectPermissionsTest() {
 		Set<Permission> perms = dbh.getObjectPermissions(TEST_APIKEY);
 		assertTrue(perms != null);
 		assertTrue(perms.size() > 0);
+		assertTrue(false);
 	}
 
 	@Test
-	public void testGetStringPermissions() {
+	public void getStringPermissionsTest() {
 		Set<String> perms = dbh.getStringPermissions(TEST_APIKEY);
 		assertTrue(perms != null);
 		assertTrue(perms.size() > 0);
+		assertTrue(false);
 	}
 
 	@Test
-	public void testGetRolePermissions() {
+	public void getRolePermissionsTest() {
 		Set<String> perms = dbh.getRolePermissions(Role.ADMIN.toString());
 		assertTrue(perms != null);
 		assertTrue(perms.size() > 0);
+		assertTrue(false);
 	}
 
 }
