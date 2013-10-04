62ebb8c3d2964440b127a1104590325b2f83820d
Matthew Frost <mfrost@stopbadware.org>
2013-10-04 10:46:05 -0400

    DATA-120 created test subject for unit tests accessing resources requiring authentication and/or authorization

--- test/org/stopbadware/dsp/data/AsnsHandlerTest.java
+++ test/org/stopbadware/dsp/data/AsnsHandlerTest.java
@@ -1,5 +1,24 @@
 package org.stopbadware.dsp.data;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.stopbadware.dsp.sec.Permissions;
+import org.stopbadware.dsp.test.helpers.AuthAuthTestHelper;
+import org.apache.shiro.subject.Subject;
+
 public class AsnsHandlerTest {
+	
+	private Subject subject = AuthAuthTestHelper.getSubject();
+	private AsnsHandler asns = new AsnsHandler(MongoDb.getDB(), subject);
+	
+	@Test
+	public void testGetAS() {
+		assertTrue(subject.isPermitted(Permissions.READ_ASNS));
+	}
+	
+	@Test
+	public void testAddAutonmousSystem() {
+		
+	}
 
 }
---
+++ test/org/stopbadware/dsp/test/helpers/AuthAuthTestHelper.java
@@ -0,0 +1,44 @@
+package org.stopbadware.dsp.test.helpers;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.mgt.DefaultSessionStorageEvaluator;
+import org.apache.shiro.mgt.DefaultSubjectDAO;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.mgt.SessionStorageEvaluator;
+import org.apache.shiro.mgt.SessionsSecurityManager;
+import org.apache.shiro.session.mgt.DefaultSessionManager;
+import org.apache.shiro.subject.Subject;
+import org.stopbadware.dsp.sec.Realm;
+import org.stopbadware.dsp.sec.RestToken;
+import org.stopbadware.lib.util.SHA2;
+
+/**
+ * Helper class to provide an authenticated subject for testing purposes. This
+ * requires the test database to have an enabled account with public key of
+ * 'TEST' and secret key of 'koJNTJMoMiu7XmRr5XV4QQ==' ('TEST' encrypted) 
+ *
+ */
+public class AuthAuthTestHelper {
+	
+	private static Realm realm = new Realm();
+	private static SecurityManager securityManager = new DefaultSecurityManager(realm);
+	
+	static {
+		SecurityUtils.setSecurityManager(securityManager);
+		((DefaultSessionManager)((SessionsSecurityManager) securityManager).getSessionManager()).setSessionValidationSchedulerEnabled(false);
+		SessionStorageEvaluator sessionDAO = ((DefaultSubjectDAO)((DefaultSecurityManager) securityManager).getSubjectDAO()).getSessionStorageEvaluator();
+		((DefaultSessionStorageEvaluator)sessionDAO).setSessionStorageEnabled(false);
+	}
+	
+	public static Subject getSubject() {
+		Subject subject = SecurityUtils.getSubject();
+		String test = "TEST";
+		long ts = 0L;
+		String sig = SHA2.get256(test+ts+test+test);
+		RestToken token = new RestToken(test, sig, test, ts);
+		subject.login(token);
+		return subject;
+	}
+
+}
