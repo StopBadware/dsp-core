abf9e05839f8cc7470882f317622320970d62d69
Matthew Frost <mfrost@stopbadware.org>
2013-10-14 13:02:26 -0400

    DATA-126 log search count and duration

--- src/org/stopbadware/dsp/rest/Search.java
+++ src/org/stopbadware/dsp/rest/Search.java
@@ -7,14 +7,19 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.stopbadware.dsp.SearchException;
 import org.stopbadware.dsp.data.DbHandler;
 import org.stopbadware.dsp.data.DbHandler.SearchType;
 import org.stopbadware.dsp.json.Error;
 import org.stopbadware.dsp.json.Response;
+import org.stopbadware.dsp.json.SearchResults;
 
 @Path("/search")
 public class Search extends SecureRest {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(Search.class);
 	
 	@GET
 	@Path("/{param}")
@@ -22,22 +27,32 @@ public class Search extends SecureRest {
 	public Response search(@PathParam("param") String searchFor) {
 		DbHandler dbh = getDbh();
 		if (dbh != null) {
+			SearchResults sr = null;
 			MultivaluedMap<String, String> params = uri.getQueryParameters();
 			try {
 				switch (searchFor) {
 				case "events":
-					return dbh.search(SearchType.EVENT_REPORT, params);
+					sr = dbh.search(SearchType.EVENT_REPORT, params);
 				case "hosts":
-					return dbh.search(SearchType.HOST, params);
+					sr = dbh.search(SearchType.HOST, params);
 				case "ips":
-					return dbh.search(SearchType.IP, params);
+					sr = dbh.search(SearchType.IP, params);
 				case "asns":
-					return dbh.search(SearchType.AS, params);
+					sr = dbh.search(SearchType.AS, params);
 				default:
 					break;
 				}
-				return httpResponseCode(NOT_FOUND);
+				
+				if (sr != null) {
+					//TODO DATA-126 get query string
+					LOG.info("{}:Returning {} results in {}ms for '{}'", sr.getCode(), sr.getCount(), sr.getDuration(), searchFor);
+					return sr;
+				} else {
+					return httpResponseCode(NOT_FOUND);
+				}
 			} catch (SearchException e) {
+				//TODO DATA-126 get query string
+				LOG.warn("{}:Searching for '{}' resulted in: {}", e.getCode(), searchFor, e.getMessage());
 				return new Error(e.getCode(), e.getMessage());
 			}
 		} else {
